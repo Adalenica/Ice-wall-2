07ce3421b1e1a20845d9fcc8d5997e08550adade
Adalenica <aidan@nonatomic.co.uk>
2025-04-22 10:05:56 +0100

    1

--- Ice-wall-2/Assets/Scripts/Money.cs
+++ Ice-wall-2/Assets/Scripts/Money.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  
+    public int money = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
