1055d110d5d27229be479e132e533bf22a6acb58
Adalenica <aidan@nonatomic.co.uk>
2025-04-29 13:23:04 +0100

    Unit spawn position
    Units spawn at a random point on the x axis

--- Ice-wall-2/Assets/Scripts/UnitSpawner.cs
+++ Ice-wall-2/Assets/Scripts/UnitSpawner.cs
@@ -5,6 +5,11 @@ public class UnitSpawner : MonoBehaviour
 	[ContextMenu("SpawnUnit")]
 	public void SpawnUnit(GameObject unitPrefab)
 	{
-		var unit = Instantiate(unitPrefab);
+		float minX = -7f;
+		float maxX = 5f;
+
+		Vector2 spawnPosition = new Vector2(Random.Range(minX, maxX), -4f);
+		
+		var unit = Instantiate(unitPrefab, spawnPosition, Quaternion.identity);
 	}
 }
