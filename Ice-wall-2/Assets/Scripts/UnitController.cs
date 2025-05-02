fe89485525b18e874f658d43e19fde5167873637
Adalenica <aidan@nonatomic.co.uk>
2025-05-02 11:39:20 +0100

    Attacking
    Units call the TakeDamage method on the wall

--- Ice-wall-2/Assets/Scripts/UnitController.cs
+++ Ice-wall-2/Assets/Scripts/UnitController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -20,12 +21,45 @@ namespace DefaultNamespace
 			_wall = FindFirstObjectByType<Wall>();
 			if (_wall != null)
 			{
-				_wall.TakeDamage(_unitData.Strength);
+				StartCoroutine(UnitMeleeAttackRoutine());
 			}
 			else
 			{
 				Debug.Log("No Wall");
 			}
+		}
+
+		private IEnumerator UnitMeleeAttackRoutine()
+		{
+			yield return MoveToWall();
+			yield return PerformAttack();
+			yield return ReturnToOrigin();
+			yield return WaitForNextAttack();
+			StartCoroutine(UnitMeleeAttackRoutine());
+		}
+
+		private IEnumerator WaitForNextAttack()
+		{
+			Debug.Log("Waiting for attack");
+			yield return new WaitForSeconds(1f);
+		}
+
+		private IEnumerator ReturnToOrigin()
+		{
+			Debug.Log("Return to origin");
+			yield return new WaitForSeconds(1f);
+		}
+
+		private IEnumerator PerformAttack()
+		{
+			_wall.TakeDamage(_unitData.Strength);
+			yield return new WaitForSeconds(1f);
+		}
+
+		private IEnumerator MoveToWall()
+		{
+			Debug.Log("MoveToWall");
+			yield return new WaitForSeconds(1f);
 		}
 	}
 }
