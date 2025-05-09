b18348a7ed9881855adea4d957ba585fcbfd319b
Adalenica <aidan@nonatomic.co.uk>
2025-05-09 13:25:11 +0100

    ShopToggle
    Added button to toggle the shop open and closed

--- Ice-wall-2/Assets/Scripts/OpenShop.cs
+++ Ice-wall-2/Assets/Scripts/OpenShop.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-	public class OpenShop
+	public class OpenShop: MonoBehaviour
 	{
-		
+		[SerializeField] private GameObject shopWindow;
+
+		public void OnMouseDown()
+		{
+			if (shopWindow != null)
+			{
+				shopWindow.SetActive(!shopWindow.activeSelf);
+			}
+		}
 	}
 }
