09e5c7b2ba0e3d7e4d3a08091bddf76060a202c1
Adalenica <aidan@nonatomic.co.uk>
2025-05-16 17:30:48 +0100

    GameOver
    Changed how level manager determines game over

--- Ice-wall-2/Assets/Scripts/LevelManager.cs
+++ Ice-wall-2/Assets/Scripts/LevelManager.cs
@@ -6,12 +6,16 @@ namespace DefaultNamespace
 	public class LevelManager: MonoBehaviour
 	{
 		[SerializeField] Wall _wall;
-
+		private int _currentWall = 0;
+		
 		public void WallDestroyed()
 		{
-			GameOver();
+			_currentWall++;
+			if (_currentWall >= 1)
+			{
+				GameOver();
+			}
 		}
-		
 		private void GameOver()
 		{
 			SceneManager.LoadScene(2);
