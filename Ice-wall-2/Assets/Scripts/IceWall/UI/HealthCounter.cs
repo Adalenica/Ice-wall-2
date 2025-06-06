8feeab8378d666475c3a9d6cde504610c0e46928
Adalenica <aidan@nonatomic.co.uk>
2025-06-06 12:42:13 +0100

    Health Counter
    Fixed health counter

--- Ice-wall-2/Assets/Scripts/IceWall/UI/HealthCounter.cs
+++ Ice-wall-2/Assets/Scripts/IceWall/UI/HealthCounter.cs
@@ -1,58 +1,25 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace IceWall.UI
 {
 	public class HealthCounter: MonoBehaviour
 	{
-		[SerializeField] private Wall _wall;
-		[SerializeField] private Wall _wall2;
-		[SerializeField] private Wall _wall3;
-		[SerializeField] private Wall _wall4;
+		[SerializeField] private Wall[] _walls;
 		[SerializeField] private LevelManager _levelManager;
 		public TextMeshProUGUI label;
 		
 		public void SetUp()
 		{
-			if (_levelManager.CurrentWall == 1f)
-			{
-				label.text = _wall2.Health.ToString();
-				_wall2.OnChanged.AddListener(UpdateLabel);
-			}
-			if (_levelManager.CurrentWall == 2f)
-			{
-				label.text = _wall3.Health.ToString();
-				_wall3.OnChanged.AddListener(UpdateLabel);
-			}
-			if (_levelManager.CurrentWall == 3f)
-			{
-				label.text = _wall4.Health.ToString();
-				_wall4.OnChanged.AddListener(UpdateLabel);
-			}
-			else
-			{
-				label.text = _wall.Health.ToString();
-				_wall.OnChanged.AddListener(UpdateLabel);
-			}
+			var index = _levelManager.CurrentWall;
+			label.text = _walls[index].Health.ToString();
+			_walls[index].OnChanged.AddListener(UpdateLabel);
 		}
+		
 		public void UpdateLabel()
 		{
-			if (_levelManager.CurrentWall == 1f)
-			{
-				label.text = _wall2.Health.ToString();
-			}
-			if (_levelManager.CurrentWall == 2f)
-			{
-				label.text = _wall3.Health.ToString();
-			}
-			if (_levelManager.CurrentWall == 3f)
-			{
-				label.text = _wall4.Health.ToString();
-			}
-			else
-			{
-				label.text = _wall.Health.ToString();
-			}
+			label.text = _walls[_levelManager.CurrentWall].Health.ToString();
 		}
 	}
 }
