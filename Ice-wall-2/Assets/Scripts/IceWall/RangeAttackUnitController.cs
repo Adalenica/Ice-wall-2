fb2a1c8a078addcd23a7000b5956cf498912707c
Adalenica <aidan@nonatomic.co.uk>
2025-06-17 15:29:31 +0100

    Blast
    Blast sprite appears when unit shoots

--- Ice-wall-2/Assets/Scripts/IceWall/RangeAttackUnitController.cs
+++ Ice-wall-2/Assets/Scripts/IceWall/RangeAttackUnitController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor.U2D;
 using UnityEngine;
 
 namespace IceWall
@@ -6,6 +7,7 @@ namespace IceWall
 	public class RangeAttackUnitController: UnitController
 	{
 		[SerializeField] private ProjectileData ProjectileData;
+		[SerializeField] private SpriteRenderer _blastSpriteRenderer;
 		private ProjectileController _projectileController;
 		
 		protected override void StartAttackRoutine()
@@ -30,6 +32,7 @@ namespace IceWall
 
 		private IEnumerator FireProjectile()
 		{
+			Blast();
 			Vector3 offset = transform.up * UnitData.ProjectileOffset;
 			var upgrade = UpgradeManager.CurrentUpgrade(UnitData);
 			var bullet = Instantiate(ProjectileData.ProjectilePrefab, transform.position + offset, Quaternion.identity);
@@ -37,6 +40,18 @@ namespace IceWall
 			projectileController.SetTarget(Wall);
 			projectileController.SetUpgrade(upgrade);
 			yield return new WaitForSeconds(1f);
+		}
+
+		private void Blast()
+		{
+			_blastSpriteRenderer.enabled = true;	
+			StartCoroutine(DisableBlastAfterDelay());
+		}
+		
+		IEnumerator DisableBlastAfterDelay()
+		{
+			yield return new WaitForSeconds(1f);
+			_blastSpriteRenderer.enabled = false;
 		}
 	}
 }
